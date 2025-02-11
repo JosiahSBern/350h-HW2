d1d66a652ff8571050285bd387d8835fe37e9445
Josiah Bernard <josiahrbernard@gmail.com>
2025-02-11 02:05:41 -0500

    Implement menu selection using switch statements

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿// the ourAnimals array will store the following: 
+﻿//HW Guided Project #3
+
+// the ourAnimals array will store the following: 
 string animalSpecies = "";
 string animalID = "";
 string animalAge = "";
@@ -19,51 +21,97 @@ string[,] ourAnimals = new string[maxPets, 6];
 // create some initial ourAnimals array entries
 for (int i = 0; i < maxPets; i++)
 {
-    if (i == 0)
-    {
-        animalSpecies = "dog";
-        animalID = "d1";
-        animalAge = "2";
-        animalPhysicalDescription = "medium sized cream colored female golden retriever weighing about 65 pounds. housebroken.";
-        animalPersonalityDescription = "loves to have her belly rubbed and likes to chase her tail. gives lots of kisses.";
-        animalNickname = "lola";
+    //If-Else to Switch
+    switch(i){
+        case 0:
+            animalSpecies = "dog";
+            animalID = "d1";
+            animalAge = "2";
+            animalPhysicalDescription = "medium sized cream colored female golden retriever weighing about 65 pounds. housebroken.";
+            animalPersonalityDescription = "loves to have her belly rubbed and likes to chase her tail. gives lots of kisses.";
+            animalNickname = "lola";
+            break;
+        
+        case 1:
+            animalSpecies = "dog";
+            animalID = "d2";
+            animalAge = "9";
+            animalPhysicalDescription = "large reddish-brown male golden retriever weighing about 85 pounds. housebroken.";
+            animalPersonalityDescription = "loves to have his ears rubbed when he greets you at the door, or at any time! loves to lean-in and give doggy hugs.";
+            animalNickname = "loki";
+            break;
+
+        case 2:
+            animalSpecies = "cat";
+            animalID = "c3";
+            animalAge = "1";
+            animalPhysicalDescription = "small white female weighing about 8 pounds. litter box trained.";
+            animalPersonalityDescription = "friendly";
+            animalNickname = "Puss";
+            break;
+
+        case 3:
+            animalSpecies = "cat";
+            animalID = "c4";
+            animalAge = "?";
+           animalPhysicalDescription = "";
+            animalPersonalityDescription = "";
+            animalNickname = "";
+            break;
+        
+        default:
+            animalSpecies = "";
+            animalID = "";
+            animalAge = "";
+            animalPhysicalDescription = "";
+            animalPersonalityDescription = "";
+            animalNickname = "";
+            break;
+
+
+        
+
     }
-    else if (i == 1)
-    {
-        animalSpecies = "dog";
-        animalID = "d2";
-        animalAge = "9";
-        animalPhysicalDescription = "large reddish-brown male golden retriever weighing about 85 pounds. housebroken.";
-        animalPersonalityDescription = "loves to have his ears rubbed when he greets you at the door, or at any time! loves to lean-in and give doggy hugs.";
-        animalNickname = "loki";
-    }
-    else if (i == 2)
-    {
-        animalSpecies = "cat";
-        animalID = "c3";
-        animalAge = "1";
-        animalPhysicalDescription = "small white female weighing about 8 pounds. litter box trained.";
-        animalPersonalityDescription = "friendly";
-        animalNickname = "Puss";
-    }
-    else if (i == 3)
-    {
-        animalSpecies = "cat";
-        animalID = "c4";
-        animalAge = "?";
-        animalPhysicalDescription = "";
-        animalPersonalityDescription = "";
-        animalNickname = "";
-    }
-    else
-    {
-        animalSpecies = "";
-        animalID = "";
-        animalAge = "";
-        animalPhysicalDescription = "";
-        animalPersonalityDescription = "";
-        animalNickname = "";
-    }
+
+    // if (i == 0)
+    // {}
+      
+    // else if (i == 1)
+    // {
+    //     animalSpecies = "dog";
+    //     animalID = "d2";
+    //     animalAge = "9";
+    //     animalPhysicalDescription = "large reddish-brown male golden retriever weighing about 85 pounds. housebroken.";
+    //     animalPersonalityDescription = "loves to have his ears rubbed when he greets you at the door, or at any time! loves to lean-in and give doggy hugs.";
+    //     animalNickname = "loki";
+    // }
+    // else if (i == 2)
+    // {
+    //     animalSpecies = "cat";
+    //     animalID = "c3";
+    //     animalAge = "1";
+    //     animalPhysicalDescription = "small white female weighing about 8 pounds. litter box trained.";
+    //     animalPersonalityDescription = "friendly";
+    //     animalNickname = "Puss";
+    // }
+    // else if (i == 3)
+    // {
+    //     animalSpecies = "cat";
+    //     animalID = "c4";
+    //     animalAge = "?";
+    //     animalPhysicalDescription = "";
+    //     animalPersonalityDescription = "";
+    //     animalNickname = "";
+    // }
+    // else
+    // {
+    //     animalSpecies = "";
+    //     animalID = "";
+    //     animalAge = "";
+    //     animalPhysicalDescription = "";
+    //     animalPersonalityDescription = "";
+    //     animalNickname = "";
+    // }
 
     ourAnimals[i, 0] = "ID #: " + animalID;
     ourAnimals[i, 1] = "Species: " + animalSpecies;
@@ -74,8 +122,11 @@ for (int i = 0; i < maxPets; i++)
 }
 
 // display the top-level menu options
-
+do{
 Console.Clear();
+
+
+
 
 Console.WriteLine("Welcome to the Contoso PetFriends app. Your main menu options are:");
 Console.WriteLine(" 1. List all of our current pet information");
@@ -94,9 +145,31 @@ if (readResult != null)
 {
     menuSelection = readResult.ToLower();
 }
+switch(menuSelection){
+    case "1":
+        break;
+    case "2":
+        break;
+
+    case "3":
+            break;
+    case "4":
+            break;
+    case "5":
+            break;
+    case "6":
+            break;
+    case "7":
+            break;
+
+    case "8":
+            break;
+}
+
 
 Console.WriteLine($"You selected menu option {menuSelection}.");
 Console.WriteLine("Press the Enter key to continue");
 
 // pause code execution
 readResult = Console.ReadLine();
+}while(menuSelection != "exit");
