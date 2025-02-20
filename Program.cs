b71861bde0846af33253d4ca78d09d5ea4dfd072
Josiah Bernard <josiahrbernard@gmail.com>
2025-02-20 02:24:15 -0500

    Create AssignGroup method

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ string[] pettingZoo =
 
 RandomizeAnimals();
 //var group = AssignGroup();
-//string[,] group = AssignGroup();
+string[,] group = AssignGroup();
 Console.WriteLine("School");
 //PrintGroup(group);
 
@@ -29,10 +29,25 @@ void RandomizeAnimals(){
         pettingZoo[r] = temp;
       
     }
-      foreach(string animal in pettingZoo) 
-        {
-        Console.WriteLine(animal);
+    //   foreach(string animal in pettingZoo) 
+    //     {
+    //     Console.WriteLine(animal);
+    //     }
+}
+
+
+string[,] AssignGroup(int groups = 6){
+
+    string[,] result = new string[groups, pettingZoo.Length/groups];
+
+    int start = 0;
+    for(int i = 0; i < groups;i++){
+        for(int j = 0; j < result.GetLength(1); j++){
+            result[i,j] = pettingZoo[start++];
+
         }
+    }
+    return result;
 }
 //AssignGroup();
 //AssignGroup();
